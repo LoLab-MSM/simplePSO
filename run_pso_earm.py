3474812601fd81b3f668a413096317c8d6fbbdbf
jamespino <james.c.pino@vanderbilt.edu>
2015-03-23 16:52:54 -0500

    moved back to estimate_m1a cost function, MOO acceptance criteria. Ran on earm_embedded. 500 iterations, 100 particles, 100 runs.

--- run_pso_earm.py
+++ run_pso_earm.py
@@ -11,7 +11,6 @@ import scipy.optimize
 import numpy
 import pylab
 from deap import base
-
 from deap import creator
 from deap import tools
 import pysb.integrate
@@ -30,103 +29,166 @@ from earm.lopez_embedded import model
 import sys
 from pysb.util import load_params
 
-best=np.loadtxt("/home/pinojc/Copy/HHMI/embedded.txt")
-#for param in model.parameters:
-#    if param.name in param_dict:
-#        param.value = param_dict[param.name]
 
-data_filename = os.path.join(os.path.dirname(__file__), 'experimental_data.npy')
 
-ydata_norm = numpy.load(data_filename)
+obs_names = ['mBid', 'cPARP']
+data_names = ['norm_ICRP', 'norm_ECRP']
+var_names = ['nrm_var_ICRP', 'nrm_var_ECRP']
+# Total starting amounts of proteins in obs_names, for normalizing simulations
+obs_totals = [model.parameters['Bid_0'].value,
+              model.parameters['PARP_0'].value]
 
-exp_var = 0.2
+earm_path = '/home/pinojc/Projects/earm'
+data_path = os.path.join(earm_path, 'xpdata', 'forfits',
+                         'EC-RP_IMS-RP_IC-RP_data_for_models.csv')
+exp_data = np.genfromtxt(data_path, delimiter=',', names=True)
 
-rate_params = model.parameters_rules()
+# Model observable corresponding to the IMS-RP reporter (MOMP timing)
+momp_obs = 'aSmac'
+# Mean and variance of Td (delay time) and Ts (switching time) of MOMP, and
+# yfinal (the last value of the IMS-RP trajectory)
+momp_obs_total = model.parameters['Smac_0'].value
+momp_data = np.array([9810.0, 180.0, momp_obs_total])
+momp_var = np.array([7245000.0, 3600.0, 1e4])
 
-tspan = np.linspace(0,5.5 * 3600,len(ydata_norm)*10)  # 5.5 hours, in seconds
-obs_names = ['mBid', 'aSmac', 'cPARP']
-# Initialize solver object
+
+
+ntimes = len(exp_data['Time'])
+tmul = 10
+tspan = np.linspace(exp_data['Time'][0], exp_data['Time'][-1],
+                    (ntimes-1) * tmul + 1)
+
 #solver = pysb.integrate.Solver(model, tspan, integrator='lsoda', rtol=1e-6, atol=1e-6, nsteps=20000)
 solver = pysb.integrate.Solver(model, tspan, integrator='vode',  with_jacobian=True,rtol=1e-5, atol=1e-5,)
-# Get parameters for rates only
-rate_params = model.parameters_rules()
 
+
+rate_params = model.parameters_rules()
 param_values = np.array([p.value for p in model.parameters])
 rate_mask = np.array([p in rate_params for p in model.parameters])
-# Build a boolean mask for those params against the entire param list
 k_ids = [p.value for p in model.parameters_rules()]
 
 
-def normalize(trajectories):
-    """Rescale a matrix of model trajectories to 0-1"""
-    ymin = trajectories.min(0)
-    ymax = trajectories.max(0)
-    return (trajectories - ymin) / (ymax - ymin)
 
-def extract_records(recarray, names):
-    """Convert a record-type array and list of names into a float array"""
-    return numpy.vstack([recarray[name] for name in names]).T
-
-def likelihood(x):
-
-    Y=np.copy(x)
+def likelihood(position):
+    Y=np.copy(position)
     param_values[rate_mask] = 10 ** Y
     solver.run(param_values)
-    ysim_array = extract_records(solver.yobs, obs_names)
-    ysim_norm = normalize(ysim_array)
-    err = numpy.sum((ydata_norm - ysim_norm[::10]) ** 2 / (2 * exp_var ** 2))
-    #err = (ydata_norm - ysim_norm) ** 2 / (2 * exp_var ** 2)
-    #err= np.sum(err,axis=0)
-    #print err
-    return err,
 
-def display(x):
-    Y=np.copy(x)
-    param_values = np.array([p.value for p in model.parameters])
-    rate_mask = np.array([p in rate_params for p in model.parameters])
+    
+    for obs_name, data_name, var_name, obs_total in \
+            zip(obs_names, data_names, var_names, obs_totals):
+        ysim = solver.yobs[obs_name][::tmul]
+        ysim_norm = ysim / obs_total
+        ydata = exp_data[data_name]
+        yvar = exp_data[var_name]
+        if obs_name == 'mBid':
+            e1 = np.sum((ydata - ysim_norm) ** 2 / (2 * yvar)) / len(ydata)
+        else:
+            e2 = np.sum((ydata - ysim_norm) ** 2 / (2 * yvar)) / len(ydata)
+
+    ysim_momp = solver.yobs[momp_obs]
+    if np.nanmax(ysim_momp) == 0:
+        print 'No aSmac!'
+        ysim_momp_norm = ysim_momp
+        t10 = 0
+        t90 = 0
+    else:
+        ysim_momp_norm = ysim_momp / np.nanmax(ysim_momp)
+        st, sc, sk = scipy.interpolate.splrep(tspan, ysim_momp_norm)
+        try:
+            t10 = scipy.interpolate.sproot((st, sc-0.10, sk))[0]
+            t90 = scipy.interpolate.sproot((st, sc-0.90, sk))[0]
+        except IndexError:
+            t10 = 0
+            t90 = 0
+
+    td = (t10 + t90) / 2
+    ts = t90 - t10
+    yfinal = ysim_momp[-1]
+    momp_sim = [td, ts, yfinal]
+    e3 = np.sum((momp_data - momp_sim) ** 2 / (2 * momp_var)) / 3
+
+    
+    return [e1,e2,e3]
+
+def display(position):
+
+
+    exp_obs_norm = exp_data[data_names].view(float).reshape(len(exp_data), -1).T
+    var_norm = exp_data[var_names].view(float).reshape(len(exp_data), -1).T
+    std_norm = var_norm ** 0.5
+    Y=np.copy(position)
     param_values[rate_mask] = 10 ** Y
     solver.run(param_values)
-    ysim_array = extract_records(solver.yobs, obs_names)
-    ysim_norm = normalize(ysim_array)
-    count=1
-    for j,obs_name in enumerate(obs_names):
-      plt.subplot(3,1,count)
-      plt.plot(solver.tspan,ysim_norm[:,j])
-      plt.plot(solver.tspan[::10],ydata_norm[:,j],'-x')
-      plt.title(str(obs_name))
-      count+=1
-    plt.ylabel('concentration')
-    plt.xlabel('time (s)')
+    obs_names_disp = obs_names + ['aSmac']
+    sim_obs = solver.yobs[obs_names_disp].view(float).reshape(len(solver.yobs), -1)
+    totals = obs_totals + [momp_obs_total]
+    sim_obs_norm = (sim_obs / totals).T
+    colors = ('r', 'b')
+    for exp, exp_err, sim, c in zip(exp_obs_norm, std_norm, sim_obs_norm, colors):
+        plt.plot(exp_data['Time'], exp, color=c, marker='.', linestyle=':')
+        plt.errorbar(exp_data['Time'], exp, yerr=exp_err, ecolor=c,
+                     elinewidth=0.5, capsize=0, fmt=None)
+        plt.plot(solver.tspan, sim, color=c)
+    plt.plot(solver.tspan, sim_obs_norm[2], color='g')
+    plt.vlines(momp_data[0], -0.05, 1.05, color='g', linestyle=':')
     plt.show()
 
+
+
+    
 def generate(size, speedmin, speedmax):
-    u1 = (random.uniform(-1., 1.) for _ in range(size))
-    tmp = map(operator.add,np.log10(k_ids),u1)
+    #u1 = (random.uniform(-1., 1.) for _ in range(size))
+    u1 = np.random.uniform(-1,1,size)
+    #tmp = map(operator.add,np.log10(k_ids),u1)
+    tmp = np.log10(k_ids) + u1
     part = creator.Particle(tmp)
-    part.speed = [random.uniform(speedmin, speedmax) for _ in range(size)]
+    #part.speed = [random.uniform(speedmin, speedmax) for _ in range(size)]
+    part.speed = np.random.uniform(speedmin,speedmax,size)
     part.smin = speedmin
     part.smax = speedmax
     return part
 
 def updateParticle(part, best, phi1, phi2):
 
-    u1 = (random.uniform(0, phi1) for _ in range(len(part)))
-    u2 = (random.uniform(0, phi2) for _ in range(len(part)))
-    v_u1 = map(operator.mul, u1, map(operator.sub, part.best, part))
-    v_u2 = map(operator.mul, u2, map(operator.sub, best, part))
-    part.speed = list(map(operator.add, part.speed, map(operator.add, v_u1, v_u2)))
+    #u1 = (random.uniform(0, phi1) for _ in range(len(part)))
+    #u2 = (random.uniform(0, phi2) for _ in range(len(part)))
+    #v_u1 = map(operator.mul, u1, map(operator.sub, part.best, part))
+    #v_u2 = map(operator.mul, u2, map(operator.sub, best, part))
+    #part.speed = list(map(operator.add, part.speed, map(operator.add, v_u1, v_u2)))
+    #part[:] = list(map(operator.add, part, part.speed))
+    u1 = numpy.random.uniform(0, phi1, len(part))
+    u2 = numpy.random.uniform(0, phi2, len(part))
+    v_u1 = u1 * (part.best - part)
+    v_u2 = u2 * (best - part)
+    part.speed += v_u1 + v_u2
+    
     for i, speed in enumerate(part.speed):
         if speed < part.smin:
             part.speed[i] = part.smin
         elif speed > part.smax:
             part.speed[i] =  part.smax
-    part[:] = list(map(operator.add, part, part.speed))
+    part += part.speed
     for i, pos in enumerate(part):
         if pos < lb[i]:
             part[i] = lb[i]
         elif pos > ub[i]:
             part[i] =  ub[i]
-
+def dominates(new, old):
+    sum_old = np.sum(old)
+    sum_new = np.sum(new)
+    diff = sum_old - sum_new
+    change1= old[0] - new[0]
+    change2= old[1] - new[1] 
+    if  change1 >= 0. and change2 >= 0.:
+        return True
+    if change1 >= 0. or change2 >=0.: 
+        if diff >= sum_old*.1:
+            return True
+        else:
+            return False
+    else:
+        return False                
 toolbox = base.Toolbox()
 nominal_values = np.array([p.value for p in model.parameters])
 xnominal = np.log10(nominal_values[rate_mask])
@@ -135,8 +197,8 @@ lb = xnominal - bounds_radius
 ub = xnominal + bounds_radius
 
 
-creator.create("FitnessMin", base.Fitness,weights=(-1.00,))
-creator.create("Particle", list, fitness=creator.FitnessMin, \
+creator.create("FitnessMin", base.Fitness,weights=(-1.00,-1.00,-1.00))
+creator.create("Particle", np.ndarray, fitness=creator.FitnessMin, \
     speed=list,smin=list, smax=list, best=None)
 toolbox.register("particle", generate, size=np.shape(rate_params)[0],\
                  speedmin=-.2,speedmax=.2)
@@ -144,11 +206,12 @@ toolbox.register("population", tools.initRepeat, list, toolbox.particle)
 toolbox.register("update", updateParticle, phi1=2, phi2=2)
 toolbox.register("evaluate", likelihood)
 
+
 stats = tools.Statistics(lambda ind: ind.fitness.values)
-stats.register("avg", numpy.mean)
-stats.register("std", numpy.std)
-stats.register("min", numpy.min)
-stats.register("max", numpy.max)
+stats.register("avg", numpy.mean, axis=0)
+stats.register("std", numpy.std, axis=0)
+stats.register("min", numpy.min, axis=0)
+stats.register("max", numpy.max, axis=0)
 logbook = tools.Logbook()
 logbook.header = ["gen", "evals"] + stats.fields
 
@@ -158,40 +221,48 @@ def init(sample,dictionary):
     Sample,Dictionary = sample,dictionary
 
 def OBJ(block):
-    #print block
     obj_values[block]=likelihood(sample[block])
 
-if __name__ == '__main__':
-    GEN = 10
-    num_particles = 50
-    pop = toolbox.population(n=num_particles)
+if "__main__":# main():
+    GEN = 500
+    num_particles = 100
+    
     best = creator.Particle(xnominal)
     best.fitness.values = likelihood(xnominal)
-    best_values =[]
-    evals = []
+    pop = toolbox.population(n=num_particles)
+    best_values =np.zeros((GEN,3))
+    evals = np.zeros(GEN)
     for g in range(1,GEN+1):
         m = mp.Manager()
         obj_values = m.dict()
         sample = []
         for p in pop:
             sample.append(p)
-        p = mp.Pool(4,initializer = init, initargs=(sample,obj_values))
+        p = mp.Pool(8,initializer = init, initargs=(sample,obj_values))
         allblocks =range(len(pop))
         p.imap_unordered(OBJ,allblocks)
         p.close()
         p.join()
         count=0
+        fitnesses = toolbox.map(toolbox.evaluate, pop)
         
+        #for ind, fit in zip(pop, fitnesses):
+        #    ind.fitness.values = fit
         for part in pop:
             part.fitness.values = obj_values[count]
+            
             count+=1
-            if g == 1:
+            if not g == 1:
+                #if part.fitness.dominates(part.best.fitness):
+                if dominates(part.fitness.values,part.best.fitness.values):
+                    part.best = creator.Particle(part)
+                    part.best.fitness.values = part.fitness.values
+            else:
                 part.best = creator.Particle(part)
                 part.best.fitness.values = part.fitness.values
-            elif part.best.fitness < part.fitness:
-                part.best = creator.Particle(part)
-                part.best.fitness.values = part.fitness.values
-            if best.fitness < part.fitness:
+
+            #if part.fitness.dominates(best.fitness):
+            if dominates(part.fitness.values,best.fitness.values):
                 best = creator.Particle(part)
                 best.fitness.values = part.fitness.values
         for part in pop:
@@ -199,18 +270,32 @@ if __name__ == '__main__':
  
         logbook.record(gen=g, evals=len(pop), **stats.compile(pop))
         print(logbook.stream),best.fitness.values
-        best_values.append(best.fitness.values)
-        evals.append(g*num_particles)
+        best_values[g-1,:] = best.fitness.values
+        evals[g-1] = g*num_particles
     
-    plt.plot(evals,best_values)
-    plt.show()
+    #plt.plot(evals,best_values[:,0])
+    #plt.show()
+    #plt.plot(evals,best_values[:,1])
+    #plt.show()
+    #plt.plot(best_values[:,0],best_values[:,1],'o')
+    #plt.show()
     #quit()
-    display(best)
+    #display(best)
+    
+    np.savetxt('%s_%s_overall_best.txt'% (sys.argv[1],'embedded'),best)
+    end_states = np.zeros((len(best),num_particles))
+    end_values = np.zeros((len(pop),3))
+    for i,part in enumerate(pop):
+        end_values[i,:] = part.best.fitness.values
+        end_states[:,i] = part.best
+    np.savetxt('%s_%s_populations_best.txt' % (sys.argv[1],'embedded'),end_states)
+    np.savetxt('%s_%s_populations_fitness.txt'% (sys.argv[1],'embedded'),end_values)
+        
 
 
 
-
-
+#import profile
+#profile.run('main()')
 
 
   
