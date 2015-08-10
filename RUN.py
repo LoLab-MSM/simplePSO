d5dfa896c9e34e882f48ad010b3b74e7c6ec0ef6
jamespino <james.c.pino@vanderbilt.edu>
2015-08-10 18:52:32 -0500

    adding stochastic analysis following pso run

--- RUN.py
+++
@@ -1,3 +0,0 @@
-import os
-for i in range(1,101):
-	os.system('python run_pso_earm.py %s >> %s.out' %(str(i),str(i)))
---
+++ mp_deap_pso_earm_stochastic_follow_up.py
@@ -0,0 +1,170 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 31 16:25:43 2014
+
+@author: james
+"""
+import scipy.optimize
+import pysb.integrate
+import pysb.util
+import numpy as np
+import scipy.interpolate
+import matplotlib.pyplot as plt
+import os
+import sys
+import earm
+import time
+from earm.lopez_embedded import model
+from pysb.tools.bngSSA import BNGSSASimulator
+
+
+obs_names = ['mBid', 'cPARP']
+data_names = ['norm_ICRP', 'norm_ECRP']
+var_names = ['nrm_var_ICRP', 'nrm_var_ECRP']
+# Total starting amounts of proteins in obs_names, for normalizing simulations
+obs_totals = [model.parameters['Bid_0'].value,
+              model.parameters['PARP_0'].value]
+
+earm_path = os.path.dirname(earm.__file__).rstrip('earm')
+#earm_path = '/home/pinojc/Copy/git/earm'
+data_path = os.path.join(earm_path, 'xpdata', 'forfits',
+                         'EC-RP_IMS-RP_IC-RP_data_for_models.csv')
+exp_data = np.genfromtxt(data_path, delimiter=',', names=True)
+
+# Model observable corresponding to the IMS-RP reporter (MOMP timing)
+momp_obs = 'aSmac'
+# Mean and variance of Td (delay time) and Ts (switching time) of MOMP, and
+# yfinal (the last value of the IMS-RP trajectory)
+momp_obs_total = model.parameters['Smac_0'].value
+momp_data = np.array([9810.0, 180.0, momp_obs_total])
+momp_var = np.array([7245000.0, 3600.0, 1e4])
+
+ntimes = len(exp_data['Time'])
+tmul = 10
+tspan = np.linspace(exp_data['Time'][0], exp_data['Time'][-1],
+                    (ntimes-1) * tmul + 1)
+
+rate_params = model.parameters_rules()
+param_values = np.array([p.value for p in model.parameters])
+rate_mask = np.array([p in rate_params for p in model.parameters])
+k_ids = [p.value for p in model.parameters_rules()]
+nominal_values = np.array([p.value for p in model.parameters])
+xnominal = np.log10(nominal_values[rate_mask])
+bounds_radius = 2
+#solver = pysb.integrate.Solver(model, tspan, integrator='vode', rtol=1e-6, atol=1e-6,)
+solver = BNGSSASimulator(model,tspan,verbose=True)
+
+def display(position):
+
+    exp_obs_norm = exp_data[data_names].view(float).reshape(len(exp_data), -1).T
+    var_norm = exp_data[var_names].view(float).reshape(len(exp_data), -1).T
+    std_norm = var_norm ** 0.5
+    Y=np.copy(position)
+    param_values[rate_mask] = 10 ** Y
+    solver.run(param_values)
+    obs_names_disp = obs_names + ['aSmac']
+    sim_obs = solver.yobs[obs_names_disp].view(float).reshape(len(solver.yobs), -1)
+    totals = obs_totals + [momp_obs_total]
+    sim_obs_norm = (sim_obs / totals).T
+    colors = ('r', 'b')
+    for exp, exp_err, sim, c in zip(exp_obs_norm, std_norm, sim_obs_norm, colors):
+        plt.plot(exp_data['Time'], exp, color=c, marker='.', linestyle=':')
+        plt.errorbar(exp_data['Time'], exp, yerr=exp_err, ecolor=c,
+                     elinewidth=0.5, capsize=0)
+        plt.plot(solver.tspan, sim, color=c)
+    plt.plot(solver.tspan, sim_obs_norm[2], color='g')
+    plt.vlines(momp_data[0], -0.05, 1.05, color='g', linestyle=':')
+    plt.show()
+
+
+
+def likelihood(position):
+    Y=np.copy(position)
+    param_values[rate_mask] = 10 ** Y
+    changes={}
+    changes['Bid_0'] = 0
+    solver.run(param_values,initial_changes=changes)
+    ysim_momp = solver.yobs[momp_obs]
+    if np.nanmax(ysim_momp) == 0:
+        ysim_momp_norm = ysim_momp
+    else:
+        return 100000,
+        #return (100000,100000,100000)
+    solver.run(param_values,verbose=True)
+    for obs_name, data_name, var_name, obs_total in \
+            zip(obs_names, data_names, var_names, obs_totals):
+        ysim = solver.yobs[obs_name][::tmul]
+        ysim_norm = ysim / obs_total
+        ydata = exp_data[data_name]
+        yvar = exp_data[var_name]
+        if obs_name == 'mBid':
+            e1 = np.sum((ydata - ysim_norm) ** 2 / (2 * yvar)) / len(ydata)
+        else:
+            e2 = np.sum((ydata - ysim_norm) ** 2 / (2 * yvar)) / len(ydata)
+    ysim_momp = solver.yobs[momp_obs]
+    if np.nanmax(ysim_momp) == 0:
+        print 'No aSmac!'
+        ysim_momp_norm = ysim_momp
+        t10 = 0
+        t90 = 0
+    else:
+        ysim_momp_norm = ysim_momp / np.nanmax(ysim_momp)
+        st, sc, sk = scipy.interpolate.splrep(tspan, ysim_momp_norm)
+        try:
+            t10 = scipy.interpolate.sproot((st, sc-0.10, sk))[0]
+            t90 = scipy.interpolate.sproot((st, sc-0.90, sk))[0]
+        except IndexError:
+            t10 = 0
+            t90 = 0
+    td = (t10 + t90) / 2
+    ts = t90 - t10
+    yfinal = ysim_momp[-1]
+    momp_sim = [td, ts, yfinal]
+    e3 = np.sum((momp_data - momp_sim) ** 2 / (2 * momp_var)) / 3
+    error = e1 + e2 +e3
+    return error,
+    #return (e1, e2, e3,)
+
+
+
+from refactored_pso import PSO
+pso = PSO()
+pso.set_cost_function(likelihood)
+pso.set_solver(solver)
+pso.set_start_position(xnominal)
+pso.set_bounds(2)
+pso.set_speed(-.25,.25)
+values = pso.run(50,100)
+#print values
+#display(pso.best)
+run = 'run_18'
+fit_filename = '%s_EARM_2_1_fit_params.txt'%run
+import pysb
+param_values[rate_mask] = 10**pso.best
+pysb.util.write_params(model, param_values, fit_filename)
+
+
+def plot_mean_min_max(name, title=None):
+    x = np.array([tr[:][name] for tr in trajectories]).T
+    if not title:
+        title = name
+    plt.figure(title)
+    plt.plot(tout.T, x, '0.5', lw=2, alpha=0.25) # individual trajectories
+    plt.plot(tout[0], x.mean(1), 'k--', lw=3, label="Mean")
+    plt.plot(tout[0], x.min(1), 'b--', lw=3, label="Minimum")
+    plt.plot(tout[0], x.max(1), 'r--', lw=3, label="Maximum")
+    solver.run(param_values)
+    plt.plot(solver.tspan,solver.yobs[name],'ko-',label='ODE')
+    plt.legend(loc=0)
+    plt.xlabel('Time')
+    plt.ylabel('Population of %s' %name)
+    plt.savefig('%s_%s.png'%(run,name))
+    plt.savefig('%s_%s.pdf'%(run,name))
+
+tout, trajectories = run_stochkit(model, tspan,param_values, n_runs=100, seed=None, verbose=True)
+#quit()
+
+plot_mean_min_max('mBid')
+plot_mean_min_max('aSmac')
+plot_mean_min_max('cPARP')
+plt.show()
