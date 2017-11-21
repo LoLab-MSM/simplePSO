fa1d02b4a9cafed5161ee8c409d2733e8f1669f3
ildefog <ildefog@puma.mc.vanderbilt.edu>
2017-11-20 22:24:28 -0600

    necro results 2

--- simplepso/examples/necro_pso.py
+++ simplepso/examples/necro_pso.py
@@ -143,7 +143,7 @@ def run_example():
     # We also must set bounds. This can be a single scalar or an array of len(start_position)
     optimizer.set_bounds(parameter_range=3)
     optimizer.set_speed(speed_min=-.5, speed_max=.5)
-    optimizer.run(num_particles=50, num_iterations=1000)
+    optimizer.run(num_particles=25, num_iterations=1000)
     print('whatever')
     if plot:
         display(start_position, optimizer.best)
