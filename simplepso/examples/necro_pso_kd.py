1097a9aa5af0f29de32bc08565c81fde40490d4a
ildefog <ildefog@puma.mc.vanderbilt.edu>
2018-01-28 15:30:46 -0600

    calibration results 65 particles, 1000 iterations

--- simplepso/examples/necro_pso_kd.py
+++ simplepso/examples/necro_pso_kd.py
@@ -216,7 +216,7 @@ def run_example():
     # We also must set bounds. This can be a single scalar or an array of len(start_position)
     optimizer.set_bounds(parameter_range=3)
     optimizer.set_speed(speed_min=-.25, speed_max=.25)
-    optimizer.run(num_particles=65, num_iterations=5000)
+    optimizer.run(num_particles=50, num_iterations=1000)
     print(optimizer.best)
     np.savetxt('optimizer_best',optimizer.best)
     # print('whatever')
