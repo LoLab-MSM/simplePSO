48e892fd46a6736566a32412a853ab5a28d11f58
geenaildefonso <geena.ildefonso@ucf.edu>
2018-07-24 14:02:47 -0500

    adding print statements in PSO code

--- simplepso/examples/necro_pso_kd_plot.py
+++ simplepso/examples/necro_pso_kd_plot.py
@@ -295,6 +295,8 @@ def run_example():
     if plot:
         display(pso.best)
     display(pso.best)
+    print(ranked_values)
+    print(pso.values)
     np.save('/home/ildefog/ParticleSwarmOptimization/simplepso/examples/ranked_values_pso', ranked_values) # param vectors for 1000 particles
     np.save('/home/ildefog/ParticleSwarmOptimization/simplepso/examples/values_cost_pso', pso.values) #cost function for each iteration of 1000 particles
 
