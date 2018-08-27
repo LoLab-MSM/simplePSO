be29cb46ccf6c94a28ee1e1d514f0d93e545ba78
ildefog <ildefog@puma.mc.vanderbilt.edu>
2018-08-27 10:06:38 -0500

    pso cal 50 parts 100 its

--- simplepso/examples/necro_pso_kd_plot.py
+++ simplepso/examples/necro_pso_kd_plot.py
@@ -299,7 +299,7 @@ def run_example():
     # We also must set bounds. This can be a single scalar or an array of len(start_position)
     optimizer.set_bounds(parameter_range=2)
     optimizer.set_speed(speed_min=-.25, speed_max=.25)
-    optimizer.run(num_particles=50, num_iterations=1000)
+    optimizer.run(num_particles=50, num_iterations=100)
     print(optimizer.best)
     np.save('optimizer_best_50_all_new_mil',optimizer.best)
 #     # print('whatever')
