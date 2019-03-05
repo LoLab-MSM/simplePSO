5502ed87a184cf7dd8b5a5bbd79ab68053a1032d
ildefog <ildefog@puma.mc.vanderbilt.edu>
2019-03-05 17:49:02 -0600

    pso kd on necro 400 iterations

--- simplepso/examples/necro_pso_kd_plot.py
+++ simplepso/examples/necro_pso_kd_plot.py
@@ -299,9 +299,9 @@ def run_example():
     # We also must set bounds. This can be a single scalar or an array of len(start_position)
     optimizer.set_bounds(parameter_range=2)
     optimizer.set_speed(speed_min=-.25, speed_max=.25)
-    optimizer.run(num_particles=25, num_iterations=200)
+    optimizer.run(num_particles=75, num_iterations=1000)
     print(optimizer.best)
-    np.save('optimizer_best_200_mar5',optimizer.best)
+    np.save('optimizer_best_1000_mar5',optimizer.best)
 #     # print('whatever')
 #     if plot:
 # 	 display(optimizer.best)
