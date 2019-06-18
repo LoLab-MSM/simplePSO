e372dcdb83e2808774885d717faee1464b9e7f7b
jamespino <james.c.pino@vanderbilt.edu>
2019-06-18 14:39:21 -0700

    Adding run_ssa to simplepso. No need to have two seperate repos. I think I can simplify the two call even more, but it will require more planning.

---
+++ examples/run_schogl_example_ssa.py
@@ -0,0 +1,109 @@
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from scipy.stats import wasserstein_distance
+
+from pysb.integrate import odesolve
+from simplepso import PSO
+
+os.environ['CUDA_VISIBLE_DEVICES'] = "1"
+from pysb.simulator import CUDASimulator
+
+
+def run_params_for_plot(params):
+    simulator.param_values = None
+    simulator.initials = None
+    traj = simulator.run(tspan, param_values=params, number_sim=num_sim)
+    return traj.dataframe[name].unstack(0).values
+
+
+def obj_function(traj_dist):
+    tmp = traj_dist[name].values[-1, :]
+    return wasserstein_distance(tmp, actual)
+
+
+def run_pso():
+    optimizer = PSO(simulator=simulator, cost_function=obj_function,
+                    start=log10_original_values, verbose=True)
+    optimizer.set_bounds(parameter_range=.2)
+    optimizer.set_speed(speed_min=-.05, speed_max=.05)
+    optimizer.run_ssa(model, num_particles=num_particles, num_iterations=200,
+                      num_sim=num_sim)
+
+    print("Ending parameters")
+    print(10 ** optimizer.best.pos)
+    return optimizer.best.pos
+
+
+def add_subplot(traj, title, params):
+    plt.title(title)
+    plt.plot(tspan, traj, '0.5', lw=2, alpha=0.25)
+    plt.plot(tspan, traj.mean(1), 'k-*', lw=3, label="Mean")
+    plt.plot(tspan, traj.min(1), 'b--', lw=3, label="Minimum")
+    plt.plot(tspan, traj.max(1), 'r--', lw=3, label="Maximum")
+    y = odesolve(model, tspan, params)
+    plt.plot(tspan, y[name], 'g--', lw=3, label="ODE")
+    plt.ylim(0, 800)
+    plt.xlabel('Time')
+    plt.ylabel('X molecules')
+
+
+def run():
+    fit_params = run_pso()
+
+    plt.figure(figsize=(9, 5))
+    plt.subplot(231)
+    add_subplot(actual_traj, "Training data", orig_values)
+
+    plt.subplot(232)
+    orig_values[rate_mask] = 10 ** noisy_start
+    start_sim = run_params_for_plot(orig_values)
+
+    add_subplot(start_sim, "Starting state", orig_values)
+
+    orig_values[rate_mask] = 10 ** fit_params
+    best_fit_sim = run_params_for_plot(orig_values)
+
+    plt.subplot(233)
+    add_subplot(best_fit_sim, "After training", orig_values)
+
+    plt.subplot(212)
+    sns.kdeplot(actual, label='actual')
+    sns.kdeplot(start_sim[-1, :], label='before')
+    sns.kdeplot(best_fit_sim[-1, :], label='after')
+
+    plt.legend()
+    plt.tight_layout()
+    savename = 'trained_model'
+    plt.savefig("{}.png".format(savename), bbox_inches='tight', dpi=300)
+    plt.show()
+    plt.close()
+
+
+if __name__ == '__main__':
+    from pysb.examples.schlogl import model
+
+    tspan = np.linspace(0, 100, 101)
+    model.parameters['X_0'].value = 250
+    name = 'X_total'
+    num_sim = 1000
+    num_particles = 8
+
+    simulator = CUDASimulator(model, tspan=tspan, verbose=False)
+
+    actual_traj = run_params_for_plot(None)
+    actual = actual_traj[-1, :]
+    rate_mask = np.array(
+        [p in model.parameters_rules() for p in model.parameters])
+
+    orig_values = np.array([p.value for p in model.parameters])
+    log10_original_values = np.log10(orig_values[rate_mask])
+    n_params = len(log10_original_values)
+    print("Ideal parameters")
+    print(log10_original_values)
+
+    noisy_start = log10_original_values + np.random.uniform(-.1, .1, n_params)
+
+    run()
--- simplepso/pso.py
+++ simplepso/pso.py
@@ -5,6 +5,8 @@ from copy import deepcopy
 
 import numpy as np
 
+from simplepso.logging import setup_logger
+
 os.environ['OMP_NUM_THREADS'] = "1"
 
 
@@ -15,6 +17,12 @@ class Particle(object):
         self.best = None
         self.smin = None
         self.smax = None
+
+
+# Used for printing below
+header = '{:<10}' + "\t".join(['{:>12}'] * 5)
+stats_header = header.format('iteration', 'best', 'mean', 'min', 'max', 'std')
+stats_output = '{:<10}' + '\t'.join(['{:>12.3f}'] * 5)
 
 
 class PSO(object):
@@ -32,13 +40,15 @@ class PSO(object):
 
 
     Notes:
-        Must set 1.) cost_function, 2.) starting position, 3.) bounds (can be vector or float)
-        To run need to supply number of particles and number of iterations. 20 particles is a good starting place.
+        Must set 1.) cost_function, 2.) starting position, 3.) bounds
+        To run need to supply number of particles and number of iterations.
+        20 particles is a good starting place.
 
     """
 
     def __init__(self, cost_function=None, start=None, num_proc=1,
-                 save_sampled=False, verbose=False, shrink_steps=True):
+                 save_sampled=False, verbose=False, shrink_steps=True,
+                 simulator=None):
         """
 
         Parameters
@@ -63,6 +73,8 @@ class PSO(object):
         else:
             self.start = None
             self.size = None
+        if simulator is not None:
+            self.simulator = simulator
         self.verbose = verbose
         self.num_proc = num_proc
         self.best = None
@@ -79,7 +91,7 @@ class PSO(object):
         self.history = []
         self.update_w = shrink_steps
         self._is_setup = False
-
+        self.log = setup_logger(verbose)
         fi = 2.05 + 2.05  # value from kennedy paper
         self.w = 2.0 / np.abs(2.0 - fi - np.sqrt(np.power(fi, 2) - 4 * fi))
 
@@ -91,17 +103,55 @@ class PSO(object):
         """ Returns the history of the run"""
         return self.history
 
-    def _update_particle_position(self, part):
-        """ Updates an individual particles position
+    def _generate(self):
+        """ Creates Particles and sets their speed """
+        part = Particle(np.random.uniform(self.lb, self.ub, self.size))
+        part.speed = np.random.uniform(self.min_speed, self.max_speed,
+                                       self.size)
+        part.smin = self.min_speed
+        part.smax = self.max_speed
+        return part
+
+    def set_cost_function(self, cost_function):
+        """ Cost function must return a scalar followed by a , (tuple)
 
         Parameters
         ----------
-        part : Particle
-
-        Returns
-        -------
+        cost_function : callable function
 
         """
+        self.cost_function = cost_function
+
+    def _setup_pso(self):
+        """ Sets up everything for PSO. Only does once """
+        if self.max_speed is None or self.min_speed is None:
+            self.set_speed()
+
+        if not self.bounds_set:
+            self.set_bounds()
+
+        if self.start is None:
+            raise Exception("Must provide a starting position\n"
+                            "Use PSO.set_start_position() \n")
+
+        if self.cost_function is None:
+            raise Exception("No cost function. "
+                            "Set with PSO.set_cost_function().")
+
+        self._is_setup = True
+
+    def _update_connected(self):
+        """ Update the population of particles"""
+        for part in self.population:
+            if part.best is None or part.best.fitness > part.fitness:
+                part.best = deepcopy(part)
+                part.best.fitness = deepcopy(part.fitness)
+            if self.best is None or self.best.fitness > part.fitness:
+                self.best = deepcopy(part)
+                self.best.fitness = deepcopy(part.fitness)
+
+    def _update_particle_position(self, part):
+        """ Updates an individual particles position """
         phi1 = 2.05
         phi2 = 2.05
         v_u1 = np.random.uniform(0, 1, self.size) * phi1 * (
@@ -118,64 +168,12 @@ class PSO(object):
             elif pos > self.ub[i]:
                 part.pos[i] = self.ub[i]
 
-    def _generate(self):
-        """ Creates Particles and sets their speed """
-        part = Particle(np.random.uniform(self.lb, self.ub, self.size))
-        part.speed = np.random.uniform(self.min_speed, self.max_speed,
-                                       self.size)
-        part.smin = self.min_speed
-        part.smax = self.max_speed
-        return part
-
-    def set_cost_function(self, cost_function):
-        """ Cost function must return a scalar followed by a , (tuple)
-
-        :param cost_function:
-        :return:
-        """
-        self.cost_function = cost_function
-
-    def _setup_pso(self):
-        """ Sets up everything for PSO. Only does once
-
-        :return:
-        """
-        if self.max_speed is None or self.min_speed is None:
-            self.set_speed()
-
-        if not self.bounds_set:
-            self.set_bounds()
-
-        if self.start is None:
-            raise ("Must provide a starting position in order to set size of "
-                   "each particle \n**** Provide PSO.set_start_position() your"
-                   "initial starting coordinates **** \nExiting due to failure")
-
-        if self.cost_function is None:
-            raise ("Must set a cost function with PSO.set_cost_function().")
-
-        if not isinstance(self.cost_function(self.start), tuple):
-            raise ("Cost function must return a tuple. An error is occuring "
-                   "when running your starting position")
-        self._is_setup = True
-
-    def update_connected(self):
-        """ Updates the population of particles according to the connected population scheme.
-
-        :return:
-        """
-        for part in self.population:
-            if part.best is None or part.best.fitness > part.fitness:
-                part.best = deepcopy(part)
-                part.best.fitness = deepcopy(part.fitness)
-            if self.best is None or self.best.fitness > part.fitness:
-                self.best = deepcopy(part)
-                self.best.fitness = deepcopy(part.fitness)
-
     def return_ranked_populations(self):
-        """ Returns population of particles
+        """ Returns a ranked population of the particles
 
-        :return: fitness array, parameter array
+        Returns
+        -------
+        np.array, np.array
         """
         positions = np.zeros(np.shape(self.population))
         fitnesses = np.zeros(len(self.population))
@@ -188,20 +186,25 @@ class PSO(object):
     def set_start_position(self, position):
         """ Set the starting position for the population of particles.
 
-        :param position: vector of parameters
-        :return:
+        Parameters
+        ----------
+        position : array
+
         """
         self.start = np.array(position)
         self.size = len(position)
 
     def set_speed(self, speed_min=-10000, speed_max=10000):
         """ Sets the max and min speed of the particles.
-        This is usually a fraction of the range of values that the particles can travel.
 
+        This is usually a fraction of the range of values that the particles
+        can travel.
 
-        :param speed_min: negative scalar
-        :param speed_max: positive scalar
-        :return:
+        Parameters
+        ----------
+        speed_min : float
+        speed_max : float
+
         """
         self.min_speed = speed_min
         self.max_speed = speed_max
@@ -209,43 +212,66 @@ class PSO(object):
     def set_bounds(self, parameter_range=None, lower=None, upper=None):
         """ Set the search space bounds that the parameters may search.
 
-        This can be a single float, in which the particles can search plus or minus the starting values around this.
-        It can also be an array that must be the same length of the starting position.
+        This can be a single float, in which the particles can search plus or
+        minus the starting values around this. It can also be an array that
+        must be the same length of the starting position.
 
-        :param parameter_range: scalar
-        :param lower: array of len(starting position)
-        :param upper: array of len(starting position)
-        :return:
+        Parameters
+        ----------
+        parameter_range : float
+        lower : array of lower bounds for parameters
+        upper : array of upper bounds for parameters
+
         """
-        assert self.start is not None, "Must provide starting array: %r" % self.start
-        all_set_to_none = False
+        if self.start is None:
+            raise Exception("Must provide starting array: %r" % self.start)
+
         if parameter_range is None and upper is None and lower is None:
-            all_set_to_none = True
-        assert all_set_to_none is False, 'Need to provide parameter range or' \
-                                         ' upper and lower bounds'
+            raise Exception('Need to provide parameter ranges')
+
         if parameter_range is None:
-            assert self.range is not None
+            if self.range is None:
+                raise Exception("Please provide parameter range")
             parameter_range = self.range
 
         if lower is None:
             lower = self.start - parameter_range
         else:
-            assert self.size == len(lower), "If providing array for " \
-                                            "bounds, must equal length of" \
-                                            " starting position"
+            if not self.size == len(lower):
+                raise Exception("Size of bounds must equal size of particle")
         if upper is None:
             upper = self.start + parameter_range
         else:
-            assert self.size == len(
-                upper), "If providing array for bounds, " \
-                        "must equal length of starting position"
+            if not self.size == len(upper):
+                raise Exception("Size of bounds must equal size of particle")
         self.lb = lower
         self.ub = upper
         self.bounds_set = True
 
+    def calc_fitness(self, traj, num_sim):
+        population_fitness = np.zeros((len(self.population)))
+        for i, part in enumerate(self.population):
+            start = i * num_sim
+            end = (i + 1) * num_sim
+            tmp_results = traj[list(range(start, end))]
+            tmp_results = tmp_results.T.unstack('simulation')
+            error = self.cost_function(tmp_results)
+            part.fitness = error
+            population_fitness[i] = error
+        return population_fitness
+
+    def get_parameters_from_population(self, num_sims, total_sims, n_params):
+        rate_vals = np.zeros((total_sims, n_params))
+        # create parameters for each particle
+        for i, part in enumerate(self.population):
+            start = i * num_sims
+            end = (i + 1) * num_sims
+            rate_vals[start:end, :] = part.pos
+        return rate_vals
+
     def run(self, num_particles, num_iterations, num_processes=1,
             save_samples=False, stop_threshold=1e-5):
-        """
+        """ Run optimization
 
         Parameters
         ----------
@@ -259,8 +285,6 @@ class PSO(object):
             if num_particles, num_iterations, and len(parameters) is large.
         stop_threshold : float
             Threshold of standard deviation of all particles cost function.
-        Returns
-        -------
 
         """
         if self._is_setup:
@@ -286,8 +310,8 @@ class PSO(object):
                 )
                 population_fitness = np.array(fitness)
                 for ind, fit in zip(self.population, population_fitness):
-                    ind.fitness = fit[0]
-                self.update_connected()
+                    ind.fitness = fit
+                self._update_connected()
                 for part in self.population:
                     self._update_particle_position(part)
                 values[g] = self.best.fitness
@@ -301,16 +325,82 @@ class PSO(object):
                     self.print_stats(g + 1, fitness=population_fitness)
 
                 if population_fitness.std() < stop_threshold:
-                    print("Stopping criteria reached.")
+                    self.log.info("Stopping criteria reached.")
                     break
+        self.values = values[:g]
+        self.history = history[:g, :]
+
+    def run_ssa(self, model, num_particles, num_iterations, num_sim,
+                save_samples=False, stop_threshold=0):
+        """ Run PSO for a stochastic simulator
+
+        Parameters
+        ----------
+        model : pysb.Model
+        num_particles : int
+        num_iterations : int
+        num_sim : int
+        save_samples : bool
+        stop_threshold : float
+
+        """
+        if self._is_setup:
+            pass
+        else:
+            self._setup_pso()
+        if self.simulator is None:
+            raise Exception("Must provide an SSA simulator to use this method")
+
+        history = np.zeros((num_iterations, len(self.start)))
+        values = np.zeros(num_iterations)
+        # self.population = self.toolbox.population(num_particles)
+        self.population = [self._generate() for _ in range(num_particles)]
+        total_sims = num_particles * num_sim
+        rate_p = model.parameters_rules()
+        rate_mask = np.array([p in rate_p for p in model.parameters])
+        nominal_values = np.array([p.value for p in model.parameters])
+        log10_original_values = np.log10(nominal_values[rate_mask])
+        all_param_vals = np.repeat([nominal_values], total_sims, axis=0)
+
+        for g in range(0, num_iterations):
+            if self.update_w:
+                self.w = (num_iterations - g) / num_iterations
+            rate_values = self.get_parameters_from_population(
+                num_sim, total_sims, len(log10_original_values)
+            )
+
+            # convert back from log10 space
+            all_param_vals[:, rate_mask] = 10 ** rate_values
+            # reset initials and param_values so simulator won't try to
+            # duplicate any
+            self.simulator.initials = None
+            self.simulator.param_values = None
+            traj = self.simulator.run(param_values=all_param_vals).dataframe.T
+            population_fitness = self.calc_fitness(traj, num_sim)
+            self._update_connected()
+            for part in self.population:
+                self._update_particle_position(part)
+
+            values[g] = self.best.fitness
+            history[g] = self.best.pos
+
+            if self.save_sampled or save_samples:
+                curr_fit, curr_pop = self.return_ranked_populations()
+                self.all_history[g, :, :] = curr_pop
+                self.all_fitness[g, :] = curr_fit
+
+            if self.verbose:
+                self.print_stats(g + 1, fitness=population_fitness)
+            if population_fitness.std() < stop_threshold:
+                self.log.info("Stopping criteria reached.")
+                break
         self.values = values[:g]
         self.history = history[:g, :]
 
     def print_stats(self, iteration, fitness):
         if iteration == 1:
-            out = '{:<10}' + "\t".join(['{:>12}'] * 5)
-            print(out.format('iteration', 'best', 'mean', 'min', 'max', 'std'))
-        out = '{:<10}' + '\t'.join(['{:>12.3f}'] * 5)
-
-        print(out.format(iteration, self.best.fitness, fitness.mean(),
-                         fitness.min(), fitness.max(), fitness.std()))
+            self.log.info(stats_header)
+        self.log.info(
+            stats_output.format(iteration, self.best.fitness, fitness.mean(),
+                                fitness.min(), fitness.max(), fitness.std())
+        )
