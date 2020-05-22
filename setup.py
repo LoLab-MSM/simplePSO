c2456eb180892a38a3cdd2ee229760776fe43560
jamescpino <james.ch.pino@gmail.com>
2020-05-21 17:03:57 -0700

    Updated version to 2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
-_MAJOR               = 1
-_MINOR               = 0
+_MAJOR = 2
+_MINOR = 0
 _MICRO = 0
 version              = '%d.%d.%d' % (_MAJOR, _MINOR, _MICRO)
 release              = '%d.%d' % (_MAJOR, _MINOR)
